f5f5a5932dd5e3730ec80843d65e1b229acccb0f
chuross <azarasi.mo.mo@gmail.com>
2016-08-05 22:12:17 +0900

    ブリッジリクエスト処理を追加

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
 import express from 'express'
 import cors from 'cors'
+import request from 'superagent'
 
 const app = express();
 const delegate = (res, callback) => {
   return callback(null, { origin: true });
+};
+const createQuery = (params) => {
+  return Object.keys(params).reduce((prev, current) => {
+    return `${prev}&${current}=${encodeURIComponent(params[current])}`;
+  }, '').substr(1);
 };
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.all('/feedly/*', cors(delegate), (req, res) => {
-  const resolvedPath = req.path.substr('/feedly'.length);
-  res.send(`catch feedly request path=${resolvedPath}`);
+const feedlyBaseUrl = 'https://cloud.feedly.com/v3';
+
+app.get('/feedly/*', cors(delegate), (req, res) => {
+  const url = `${feedlyBaseUrl}/${req.params[0]}?${createQuery(req.query)}`;
+  request.get(url).end((error, data) => {
+    console.log(data.header['content-type']);
+    res.status = data.status;
+    res.set('Content-Type', data.header['content-type']);
+    res.send(data.text);
+  });
 });
 
 const server = app.listen(3000, () => {
