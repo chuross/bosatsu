b1d2fe56e00974ca16e8309de435982b5a31e8bc
chuross <azarasi.mo.mo@gmail.com>
2016-08-05 15:25:15 +0900

    feedly用のパスを定義した

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
+const delegate = (res, callback) => {
+  return callback(null, { origin: true });
+};
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/feedly/*', cors(delegate), (req, res) => {
+  const resolvedPath = req.path.substr('/feedly'.length);
+  res.send(`catch feedly request path=${resolvedPath}`);
 });
 
 const server = app.listen(3000, () => {
