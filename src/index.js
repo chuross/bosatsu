0c6db237cf286fc6de39fe1b1da9290eaffc76d9
chuross <azarasi.mo.mo@gmail.com>
2017-04-12 20:22:56 +0900

    汎用的なブリッジリクエストを追加した

--- src/index.js
+++ src/index.js
@@ -11,21 +11,33 @@ const createQuery = (params) => {
     return `${prev}&${current}=${encodeURIComponent(params[current])}`;
   }, '').substr(1);
 };
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-const feedlyBaseUrl = 'https://cloud.feedly.com/v3';
-
-app.get('/feedly/*', cors(delegate), (req, res) => {
-  const url = `${feedlyBaseUrl}/${req.params[0]}?${createQuery(req.query)}`;
+const exec = (url, req, res) => {
   request.get(url).end((error, data) => {
     console.log(data.header['content-type']);
     res.status = data.status;
     res.set('Content-Type', data.header['content-type']);
     res.send(data.text);
   });
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/http/*', cors(delegate), (req, res) => {
+  const url = `http://${req.params[0]}?${createQuery(req.query)}`;
+  exec(url, req, res);
+})
+
+app.get('/https/*', cors(delegate), (req, res) => {
+  const url = `http://${req.params[0]}?${createQuery(req.query)}`;
+  exec(url, req, res);
+})
+
+const feedlyBaseUrl = 'https://cloud.feedly.com/v3';
+app.get('/feedly/*', cors(delegate), (req, res) => {
+  const url = `${feedlyBaseUrl}/${req.params[0]}?${createQuery(req.query)}`;
+  exec(url, req, res);
 });
 
 const server = app.listen(3000, () => {
